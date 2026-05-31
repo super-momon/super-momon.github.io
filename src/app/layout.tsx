d4ec1c2775d400aad7efbd5377e40848287dce8c
super-momon <raymond.ayade@gmail.com>
2026-05-31 12:07:23 +0800

    refactor layout to include Google Analytics tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,20 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3EYYVGG42"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C3EYYVGG42');
+            `,
+          }}
+        />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
